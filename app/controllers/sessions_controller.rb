335e599739261021a4d6b143622b3bdb99bf48df
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-25 09:21:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:name] = params[:username]
     if session[:name]
-      session[:name] = params[:name]
       redirect_to '/'
     else
       redirect_to login_path
