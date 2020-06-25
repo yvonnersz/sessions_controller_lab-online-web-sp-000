2c5cef35f29b113eea8346fc89b6bc4ac256bc79
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-25 09:32:55 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+    if !params[:name].present?
+      redirect_to login_path
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
